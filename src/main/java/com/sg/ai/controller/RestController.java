476066b738029785805e7ab6cd95ce1e2bf6b1bb
user <user@310-32>
2025-10-17 12:21:29 +0900

    2025-10-17

---
+++ src/main/java/com/sg/ai/controller/RestController.java
@@ -0,0 +1,33 @@
+package com.sg.ai.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import com.sg.ai.domain.User;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+	@GetMapping("/jblog")
+	public User httpGet() {
+		User finduser = User.builder().id(1).username("ai").password("222").email("g@gmail.com").build();
+		return finduser;
+	}
+	
+	@PostMapping("/jblog")
+	public String httpPost() {
+		return "Post 요청";
+	}
+	
+	@PutMapping("/jblog")
+	public String httpPut() {
+		return "Put 요청";
+	}
+	
+	@DeleteMapping("/jblog")
+	public String httpDelete() {
+		return "Delete 요청";
+	}
+}
